b89ff7b14e204a5736e7ad19424f2032d3534a75
TamasBajnok <toto20030503@gmail.com>
2024-09-17 21:45:18 +0200

    hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,27 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // eltér a szögletes zárójelben lévő változó neve
-    int *b = new int[NELEMENTS];
-    // nincs pontos vessző a sor végén
-    std::cout << '1-100 ertekek duplazasa'
-        // a for ciklus feltételéből hiányzik, az hogy meddig menjen, illetve hogy mekkora lépéközökkel
-        for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    std::cout << '1-100 ertekek duplazasa';
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        // nincs pontos vessző a sor végén
-        std::cout << "Ertek:"
+
+        std::cout << "Ertek:";
     }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    // sima vessző helyett pontos vessző kell
-    for (int i = 0; i < N_ELEMENTS, i++)
+
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // nincs pontos vessző a sor végén
-        atlag += b[i]
+
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
