afc2613aaed0f07b753af53794d0214d34b3521e
TamasBajnok <toto20030503@gmail.com>
2024-09-17 21:37:01 +0200

    hibák kommentezve

--- main.cpp
+++ main.cpp
@@ -4,20 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    // eltér a szögletes zárójelben lévő változó neve
     int *b = new int[NELEMENTS];
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+        // a for ciklus feltételéből hiányzik, az hogy meddig menjen, illetve hogy mekkora lépéközökkel
+        for (int i = 0;)
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
         std::cout << "Ertek:"
-    }    
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        // nincs pontos vessző a sor végén
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
